f41f2f8903537a8d749b5ff58cf9a49895f11c5b
Michal Bobula <michal.bobula@microsoft.wsei.edu.pl>
2023-03-04 13:40:14 +0100

    init app

---
+++ WSEI_web_app/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { UsersListComponent } from './users-list/users-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsersListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
